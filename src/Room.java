c2f15d0354f1698da5859a61ec77129aa7447bd1
MRKazemi00 <mrk.reza000@gmail.com>
2019-11-26 21:11:55 +0100

    First commit made base of room class

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,36 @@
 public class Room {
+
+    private int roomNumber;
+    private int numberOfBeds;
+    private boolean hasBalcony;
+    private double pricePerNight;
+    private boolean isBooked;
+
+    public Room(int roomNumber, int numberOfBeds,boolean hasBalcony,double pricePerNight,boolean isBooked){
+        this.roomNumber = roomNumber;
+        this.numberOfBeds = numberOfBeds;
+        this.hasBalcony = hasBalcony;
+        this.pricePerNight = pricePerNight;
+        this.isBooked = isBooked;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public boolean getBooked() {
+        return isBooked;
+    }
+
+    public boolean getHasBalcony() {
+        return hasBalcony;
+    }
 }
