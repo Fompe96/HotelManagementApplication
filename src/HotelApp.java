04e3278e8310324d9fae717bb35d30b7d60d51a0
MRKazemi00 <mrk.reza000@gmail.com>
2019-12-01 20:36:00 +0100

    Fixat show menu helt och hållet

--- src/HotelApp.java
+++ src/HotelApp.java
@@ -1,17 +1,27 @@
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class HotelApp {
     Scanner input = new Scanner(System.in);
 
+
     public static void main(String[] args) {
 
-        // temporary object with method call used to try "available rooms in time period."
+        /* temporary object with method call used to try "available rooms in time period."
         HotelApp myHotel = new HotelApp();
         myHotel.availableInTimeMenu();
+
+         */
+
+
+        HotelApp myHotel = new HotelApp();
+        myHotel.showMenu();
+
     }
+
     // menu for "available rooms in time period"
-    private void availableInTimeMenu(){
+    private void availableInTimeMenu() {
         HotelApp myHotel = new HotelApp();
         HotelLogic myApp = new HotelLogic();
 
@@ -33,17 +43,17 @@ public class HotelApp {
             System.out.print("dd: ");
             dd = input.nextInt();
             LocalDate out = LocalDate.of(yyyy, mm, dd);
-            if (in.compareTo(LocalDate.now()) < 0){
+            if (in.compareTo(LocalDate.now()) < 0) {
                 System.out.println("It is not possible to check in too a date in history, try again.");
                 myHotel.availableInTimeMenu();
-            }else if (out.compareTo(LocalDate.now().plusYears(25)) > 0){
+            } else if (out.compareTo(LocalDate.now().plusYears(25)) > 0) {
                 System.out.println("maximum time to book in the future is 25 years, try again.");
                 myHotel.availableInTimeMenu();
-            }else{
-               System.out.println(myApp.availableInTime(in, out));
+            } else {
+                System.out.println(myApp.availableInTime(in, out));
             }
-        }catch (Exception e){
-           System.out.println("Something went wrong, try again.");
+        } catch (Exception e) {
+            System.out.println("Something went wrong, try again.");
             myHotel.availableInTimeMenu();
         }
 
@@ -67,4 +77,56 @@ public class HotelApp {
 
         Customer customerInfo = new Customer(customerName, customerSSN, customerAdress, customerPhoneNumber);
         customers.add(customerInfo);
-}
+    }
+
+
+    public void showMenu() {
+
+        int userChoice;
+
+        do {
+            System.out.println("Press a number according to the action you want to make");
+            System.out.println("1.Add Room");
+            System.out.println("2.Add new customer");
+            System.out.println("3.Make booking");
+            System.out.println("4.View available rooms in period");
+            System.out.println("5.Check in");
+            System.out.println("6.Check out");
+            System.out.println("9.Exit program");
+
+            userChoice = input.nextInt();
+            
+            switch (userChoice) {
+                case 1:
+                    //Add Room method
+                    break;
+                case 2:
+                    //Add NewCustomer method
+                    break;
+                case 3:
+                    //Add newBooking method
+                    break;
+                case 4:
+                    //Add VARIP method
+                    break;
+                case 5:
+                    //Add Check in method
+                    break;
+                case 6:
+                    //Add check out method
+                    break;
+                case 9:
+                    System.out.println("Have a nice day!");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid input try again");
+                    break;
+            }
+
+        }while (userChoice !=  9);
+
+    }
+
+        }
